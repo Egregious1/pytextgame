e6dfc7a8af4f8989e520b8fb300b4d888ed8592a
Brian Kahler <briankahler.designs@gmail.com>
2021-07-01 22:34:46 -0700

    changed the input validator and started coding the individual settings.

--- first-game.py
+++ first-game.py
@@ -24,20 +24,29 @@ def displayIntro():
     print('before you stands a large door with a man looking at you through a hole.')
     print('Who are you?')
     print(f'You call out, "It is I {playerName}, I have come back home!"')
+    print(f'{playerName}?! Well why didnt you say so? Come in come in!')
 
-# path selection - keeping it simple now until I can figure out how to make it better
-# this will only accept the inputs required but has no functin past verifying input atm
+#putting different rooms in this section
+def smellyMareintro():
+    print()
+    print('From outside you can hear the whole tavern is a buzz with life,')
+    print('as you take your first steps towards the door an arguing couple shuffles out of the door')
+    print('The doors open wide and you are greeted with a blast of warmth and voices')
+    print('The sound of merryment and music can beheard loudly in the streets.')
+    print('you enter the Smelly Mare unsure what to expect')
 
+# path selection - this code is going to suck but I need to write it for each room until I learn how to optimize it.
 
-def choosePath():
-    path = input('Do you go left or right? ("L" or "R"')
+def introPath():
+    path = input('Do you go left or right? ("L" or "R") ')
     if path == 'L' or path == 'l':
-        print('You turn to your left and continue down your path')
+        print('You turn to your left towards the tavern')
+        smellyMareintro()
     elif path == 'R' or path == 'r':
         print('You look to the path on your right and take it')
     else:
         if path != 'l' or path != 'L' or path != 'r' or path != 'R':
-            choosePath()
+            introPath()
 
 
 startScreen()
@@ -49,3 +58,12 @@ print()
 print(f'Welcome {playerName}')
 print()
 displayIntro()
+print()
+print('You step through the gates and to your surprise the town is quiter than expected.')
+print('You look to your left and see the Smelly Mare the local inn')
+print('to your right you see the town bizaar')
+print()
+print('Which way do you go? (L or R?)')
+introPath()
+
+
